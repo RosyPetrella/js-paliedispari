75206b55659dd1d561d40cf465f8bfa7b3da25cc
RosyPetrella <petrellamr@gmail.com>
2025-02-14 14:21:36 +0100

    add function for Palindrome ex.

--- main.js
+++ main.js
@@ -2,17 +2,26 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
+function isAPalindrome(word) {
+  let reversedWord = "";
+  for (let i = userWord.length - 1; i >= 0; i--) {
+    reversedWord += userWord[i];
+  }
+
+  if (userWord === reversedWord) {
+    return true;
+  }
+}
+
 const userWord = prompt("Type a word");
 let reversedWord = "";
 
-for (let i = userWord.length - 1; i >= 0; i--) {
-  reversedWord += userWord[i];
-}
+isAPalindrome(userWord);
 
-if (userWord === reversedWord) {
-  console.log("The word " + userWord + " is a palindrome!");
+if (isAPalindrome(userWord)) {
+  console.log(userWord + " is a palindrome");
 } else {
-  console.log("The word " + userWord + " is not a palindrome!");
+  console.log(userWord + " is NOT a palindrome");
 }
 
 // Pari e Dispari
